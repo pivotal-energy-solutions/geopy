27e5cca00b93e1dcc5db92c218d44e6a81b74fdc
Steven Klass <rh0dium@users.noreply.github.com>
2018-04-24 14:33:12 -0600

    Bug fix unicode goes first.

--- geopy/__init__.py
+++ geopy/__init__.py
@@ -1,12 +1,12 @@
+from __future__ import unicode_literals
+
 from geopy.point import Point
 from geopy.location import Location
 from geopy import geocoders
 
-from __future__ import unicode_literals
-
 __name__ = 'geopy'
 __author__ = 'Brian Beck'
-__version_info__ = (1, 0, '0rc1')
+__version_info__ = (1, 0, '0rc2')
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '2014/07/22 4:47:00 PM'
 __credits__ = ['Brian Beck', 'Steven Klass', 'Tim Valenta']
